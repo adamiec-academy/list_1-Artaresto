c8db70ba0dcaf4c51b0003ae4648c0614a6cdc3f
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-14 18:23:07 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,1 +1,9 @@
-
+def cross(n):
+    for i in range(n):
+        print(" " * n + n * "*")
+        i=i+1
+        if i==n:
+            for i in range(n):
+                print(n*(n-1)*"*")
+    for i in range(n):
+        print(" " * n + n * "*")
