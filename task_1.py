3d70381e2fd51a575cd39c5938469944fd426b13
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-14 18:16:17 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,12 @@
-def cross(n):
-    pass
+def cross(n, shift):
+    for i in range(n):
+        print(shift * " ", end="")
+        print(n * "*")
+        i=i+1
+        if i==n:
+            for i in range(n):
+                print(n*(n-1)*"*")
+    for i in range(n):
+        print(shift * " ", end="")
+        print(n * "*")
+cross(4, 4)
