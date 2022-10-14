a7e121ee8b9c3b43a9569afe4b8b5b6bd2c9af3b
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-14 18:37:14 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,9 +1,10 @@
 def cross(n):
     for i in range(n):
-        print(" " * n + n * "*")
+        print(" " * n + n * "*"+ n * " ")
         i=i+1
         if i==n:
             for i in range(n):
                 print(n*3*"*")
     for i in range(n):
-        print(" " * n + n * "*")
+        print(" " * n + n * "*" + n * " " )
+        
