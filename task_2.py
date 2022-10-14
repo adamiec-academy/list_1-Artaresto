21190b0aa01a079d82bcaab5cebb9c3047ca6abc
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-14 20:33:29 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,11 @@
 def chess_board(n, k):
-    pass
+    for i in range(k):
+        for j in range(n):
+            if (i + j) % 2 == 0:
+                print(("  "+"##")*n, end="\n")
+                print(("  "+"##")*n, end="\n")
+
+            else:
+                print(("##"+"  ")*n, end="\n")
+                print(("##"+"  ")*n, end="\n")
+                
