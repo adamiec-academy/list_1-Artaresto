df9ad349de855fd5a2cb0e312643842269d77c73
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-15 10:13:37 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,11 +1,12 @@
 def envelope(n):
     
-    for i in range (n):
-        for j in range (n):
-            if i == 0 or i == (n-1) or j==0 or j==(n-1) or i+j==(n-1) or i==j:
+    max_min = 2*n+1
+    
+    for i in range (max_min):
+        for j in range (max_min):
+            if i == 0 or i == (max_min-1) or j==0 or j==(max_min-1) or i+j==(max_min-1) or i==j:
                 print("*", end="")
             else:
                 print(" ", end="")
         
         print()
-        
